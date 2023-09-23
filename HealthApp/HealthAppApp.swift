87aeddd1d15a630273b6c0757cbe211b624807e2
Berkay <berkaysutlu94@gmail.com>
2023-09-23 13:10:02 +0300

    Initial Commit

---
+++ HealthApp/HealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthAppApp.swift
+//  HealthApp
+//
+//  Created by Berkay Sutlu on 23.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
